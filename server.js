e2ce006a7d9fa2e3b8f9083408e3095576f29f2f
Kamal <mkmbhs@gmail.com>
2017-06-04 14:32:31 +0200

    intitial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+
+
+app.use((req,res, next) => {
+  var now = new Date().toString();
+  var log = ` mkmbhs:% ${now} \n mkmbhs:% ${req.method}\n mkmbhs:% ${req.url}`;
+  next();
+
+  console.log(log)
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  });
+});
+
+
+ app.get('/', (req, res) => {
+   //res.send('<h2>Hello Express!</h1>');
+   res.render('home.hbs', {
+     welcomeMessage: 'Welcome To Mkmbhs Website',
+     pageTitle: 'Mkmbhs',
+     currentYear: new Date().getFullYear(),
+   })
+ });
+ app.get('/about', (req, res) => {
+   res.render('about.hbs', {
+     pageTitle: 'about props',
+     currentYear: new Date().getFullYear(),
+   });
+
+ });
+
+ app.get('/bad', (req, res) => {
+   res.send({
+     errorMessage : 'Error Handling you request...',
+   });
+ });
+ app.listen(3000, () => {
+   console.log('Server is up on port 3000');
+ });
